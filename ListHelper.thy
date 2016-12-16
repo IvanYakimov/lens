8ee5f1a681f47be87ddd0e08a1828167342b9c10
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-16 21:07:24 +0700

    clean up

--- ListHelper.thy
+++ ListHelper.thy
@@ -2,11 +2,13 @@ theory ListHelper
 imports Main List
 begin
 
+(*
 lemma "P x \<Longrightarrow> takeWhile P (x # xs) = x # (takeWhile P xs)"
  by (induct xs) auto
 
 lemma takeWhile_incremental: "P x \<Longrightarrow> length (takeWhile P (x # xs)) = length (takeWhile P xs) + 1"
  by (induct xs) auto
+*)
 
 (*
 lemma "\<lbrakk>\<And>x. x : set xs \<Longrightarrow> P x; \<not> P x\<rbrakk> \<Longrightarrow> takeWhile P xs = xs" 
