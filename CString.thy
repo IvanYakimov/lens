b949d3b88b699172d3947464380d1e347aafde0e
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-16 20:53:16 +0700

    Add some lemmas and simplification rules to the c-string theory.
    For each definition an appropriate 'simplification' lemma (rule) is added.
    Also proofs for some properties are provided.

--- CString.thy
+++ CString.thy
@@ -1,17 +1,46 @@
 theory CString
-imports String
+imports Main String
 begin
 
-(* Author: Ivan Yakimov, Siberian Federal University, e-mail: ivan.yakimov.research@yandex.ru *)
+(* Author: Ivan Yakimov, e-mail: ivan.yakimov.research@yandex.ru *)
 
 type_synonym string = "char list"
 
-definition terminator :: "char" where "terminator = Char Nibble0 Nibble0"
+definition terminator :: "char" where 
+"terminator = Char Nibble0 Nibble0"
 
-definition isCString :: "string \<Rightarrow> bool" where
+lemma terminator_simp[simp]:"terminator = Char Nibble0 Nibble0" by (simp add: terminator_def)
+
+definition isCString :: "string \<Rightarrow> bool" where 
 "isCString xs = (terminator \<in> set xs)"
+
+lemma isCString_simp[simp]:"isCString xs = (terminator \<in> set xs)" by (simp add: isCString_def)
 
 definition takeCString :: "char list \<Rightarrow> char list" where
 "takeCString xs = (takeWhile (\<lambda>x. x \<noteq> terminator) xs)"
+
+lemma takeCString_simp[simp]: "takeCString xs = (takeWhile (\<lambda>x. x \<noteq> terminator) xs)" by (simp add: takeCString_def)
+
+(* TODO: = instead of \<Longrightarrow> *)
+lemma "\<not> isCString xs \<Longrightarrow> List.find (\<lambda>x. x = terminator) xs = None"
+ by (induct xs) auto
+
+(* TODO: better to modify the takeCString function to return [] in such cases *)
+lemma "\<not>(isCString xs) \<Longrightarrow> takeCString xs = xs" by auto 
+
+(* support *)
+definition str_hello :: "string" where
+"str_hello = [CHR ''H'', CHR ''e'', CHR ''l'', CHR ''l'', CHR ''o'', CHR ''!'', terminator]"
+
+definition str_el :: "string" where
+"str_el = [CHR ''e'', CHR ''l'', terminator]"
+
+definition bad_str :: "string" where
+"bad_str = [CHR ''b'', CHR ''a'', CHR ''d'']"
+
+definition empty_str :: "string" where
+"empty_str = [terminator]"
+
+value "str_hello"
 
 end
