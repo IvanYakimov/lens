8f43ccf33ae0ac83e6c2b7b3099bfc571547c532
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-17 12:12:00 +0700

    Improve definition of function returning text of well-formed c-string An appropriate check is added, some lemmas are proved, more adequate behavior on non-c-string buffers is implemented.

--- CString.thy
+++ CString.thy
@@ -17,16 +17,20 @@ definition isCString :: "string \<Rightarrow> bool" where
 lemma isCString_simp[simp]:"isCString xs = (terminator \<in> set xs)" by (simp add: isCString_def)
 
 definition takeCString :: "char list \<Rightarrow> char list" where
-"takeCString xs = (takeWhile (\<lambda>x. x \<noteq> terminator) xs)"
+"takeCString xs = (if isCString xs then takeWhile (\<lambda>x. x \<noteq> terminator) xs else [])"
 
-lemma takeCString_simp[simp]: "takeCString xs = (takeWhile (\<lambda>x. x \<noteq> terminator) xs)" by (simp add: takeCString_def)
+lemma takeCString_simp[simp]: 
+"takeCString xs = (if isCString xs then takeWhile (\<lambda>x. x \<noteq> terminator) xs else [])"  by (simp add: takeCString_def)
 
 (* TODO: = instead of \<Longrightarrow> *)
 lemma "\<not> isCString xs \<Longrightarrow> List.find (\<lambda>x. x = terminator) xs = None"
  by (induct xs) auto
 
 (* TODO: better to modify the takeCString function to return [] in such cases *)
-lemma "\<not>(isCString xs) \<Longrightarrow> takeCString xs = xs" by auto 
+lemma takeCString_nil[simp]:"takeCString [] = []" by simp
+lemma takeCString_bad[simp]:"\<not>(isCString xs) \<Longrightarrow> takeCString xs = []" by simp
+lemma takeCString_well[simp]:"isCString xs \<Longrightarrow> \<exists>ys. takeCString xs = ys" by simp
+lemma takeCString_inc[simp]:"\<lbrakk>isCString xs; x \<noteq> terminator\<rbrakk> \<Longrightarrow> x # (takeCString xs) = takeCString (x # xs)" by (induct xs) auto 
 
 (* support *)
 definition str_hello :: "string" where
