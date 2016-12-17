f3decf808525d21e93f1b6e669b9897368659a43
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-17 13:24:53 +0700

    Add function takeUpto (deprecated).

--- ListHelper.thy
+++ ListHelper.thy
@@ -3,6 +3,12 @@ imports Main List
 begin
 
 (*
+primrec takeUpto :: "('a => bool) => 'a list => 'a list" where
+"takeUpto P [] = []" |
+"takeUpto P (x # xs) = (if P x then x # takeUpto P xs else x # [])"
+
+value "takeUpto (\<lambda>x. x \<noteq> 0) [1,2,3,4,5,8,0,5,6,5,4,435::int]"
+
 lemma "P x \<Longrightarrow> takeWhile P (x # xs) = x # (takeWhile P xs)"
  by (induct xs) auto
 
