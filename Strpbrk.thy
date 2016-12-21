2db8a7f72a56920e8ccc9dce04d57d8efaf12ec3
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-21 10:51:11 +0700

    Introduce model for a strpbrk function.
    The model provides correct behavior for correct c-strings.
    If args are not c-strings the behavior is considered as not defined.
    Note that there are some cases in which "real" (implemented in C language) strpbrk returns some value for incorrect arguments without failure. For example, for strings A = "abcd\0" and C = "c", where C is not-terminated string the "real" strpbrk returns "cd\0".
    On the other hand, in terms of model the behavior for strings A and C is not defined and it returns None.

--- Strpbrk.thy
+++ Strpbrk.thy
@@ -1,5 +1,48 @@
 theory Strpbrk
-imports "$PWD/CString"
+imports Main List "$PWD/CString"
 begin
 
+definition list_pbrk :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"list_pbrk xs ys = dropWhile (\<lambda>x. x \<notin> set ys) xs"
+
+lemma list_pbrk_simp[simp]:
+"list_pbrk xs ys = dropWhile (\<lambda>x. x \<notin> set ys) xs"
+ by (simp add: list_pbrk_def)
+ 
+lemma list_pbrk_inter[simp]:
+"set xs \<inter> set ys = {} \<equiv> list_pbrk xs ys = []"
+ by (induct ys) auto
+
+lemma list_pbrk_nil[simp]:
+"\<forall>y \<in> set ys. y \<notin> set xs \<Longrightarrow> list_pbrk xs ys = []" 
+ by auto
+
+lemma list_pbrk_inc[simp]:
+"\<lbrakk>list_pbrk (zs @ ws) ys = ws; z \<notin> set ys\<rbrakk> \<Longrightarrow> list_pbrk (z#zs @ ws) ys = ws"
+ by (induct zs) auto
+ 
+definition strpbrk :: "char list \<Rightarrow> char list \<Rightarrow> char list option" where
+"strpbrk xs ys = (
+ let ws = list_pbrk (takeCString xs) (takeCString ys) in (
+  if isCString xs \<and> isCString ys
+  then Some ws
+  else None))"
+
+lemma strpbrk_simp[simp]:
+"strpbrk xs ys = (
+ let ws = list_pbrk (takeCString xs) (takeCString ys) in (
+  if isCString xs \<and> isCString ys
+  then Some ws
+  else None))"
+ by (simp add: strpbrk_def)
+ 
+(* TODO: improve *)
+lemma strpbrk_nil[simp]:
+"isCString xs \<Longrightarrow> isCString ys \<Longrightarrow> \<forall>y \<in> set ys. y \<notin> set xs \<Longrightarrow> the (strpbrk xs ys) = []" 
+ by auto
+
+
+ 
+
+  
 end
