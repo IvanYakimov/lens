b26e9791074fc46e4859dd01ddb8ef41cd43eb87
Ivan Yakimov <ivan.yakimov.yandex@yandex.ru>
2016-12-21 16:11:13 +0700

    Prove additinal lemmas about helper for the strpbrk function. Refactor and clean-up the source code.
    Some lemmas about list_pbrk connection with '@' and '#' has been proved.
    Relationship between list_pbrk and strpbrk has been clarified.

--- Strpbrk.thy
+++ Strpbrk.thy
@@ -21,28 +21,60 @@ lemma list_pbrk_inc[simp]:
 "\<lbrakk>list_pbrk (zs @ ws) ys = ws; z \<notin> set ys\<rbrakk> \<Longrightarrow> list_pbrk (z#zs @ ws) ys = ws"
  by (induct zs) auto
  
+lemma list_pbrk_inc2[simp]:
+"\<lbrakk>x \<notin> set ys; list_pbrk xs ys = ws\<rbrakk> \<Longrightarrow> list_pbrk (x#xs) ys = ws"
+by (induct xs) auto
+
+lemma list_pbrk_inc3[simp]:
+"set xs \<inter> set ys = {} \<Longrightarrow> u \<in> set ys \<Longrightarrow> list_pbrk (xs @ [u] @ ws) ys = [u] @ ws"
+by (induct xs) auto
+
+lemma list_pbrk_inc4[simp]:
+"set xs \<inter> set ys = {} \<Longrightarrow> u \<in> set ys \<Longrightarrow> list_pbrk (xs @ (u#ws)) ys = u#ws"
+by (induct xs) auto
+ 
 definition strpbrk :: "char list \<Rightarrow> char list \<Rightarrow> char list option" where
 "strpbrk xs ys = (
- let ws = list_pbrk (takeCString xs) (takeCString ys) in (
   if isCString xs \<and> isCString ys
-  then Some ws
-  else None))"
+  then Some (list_pbrk (takeCString xs) (takeCString ys))
+  else None)"
 
 lemma strpbrk_simp[simp]:
 "strpbrk xs ys = (
- let ws = list_pbrk (takeCString xs) (takeCString ys) in (
   if isCString xs \<and> isCString ys
-  then Some ws
-  else None))"
+  then Some (list_pbrk (takeCString xs) (takeCString ys))
+  else None)"
  by (simp add: strpbrk_def)
  
 (* TODO: improve *)
+lemma strpbrk_inter[simp]: 
+"\<lbrakk>isCString xs; isCString ys\<rbrakk> \<Longrightarrow> set (takeCString xs) \<inter> set (takeCString ys) = {} \<equiv> the (strpbrk xs ys) = []"
+ by (induct xs)  auto
+
 lemma strpbrk_nil[simp]:
 "isCString xs \<Longrightarrow> isCString ys \<Longrightarrow> \<forall>y \<in> set ys. y \<notin> set xs \<Longrightarrow> the (strpbrk xs ys) = []" 
  by auto
-
-
  
+lemma strpbkr_nil2:
+"strpbrk [] ys = None"
+by auto
 
-  
+lemma strpbrk_nil3[simp]:
+"strpbrk xs [] = None"
+by auto
+
+lemma strpbrk_notNone[simp]:
+"\<lbrakk>isCString xs; isCString ys; set (takeCString xs) \<inter> set (takeCString ys) \<noteq> {}\<rbrakk> \<Longrightarrow> strpbrk xs ys \<noteq> None"
+by (induct xs)  auto 
+
+lemma strpbrk_some[simp]:
+"\<lbrakk>isCString xs; isCString ys; set (takeCString xs) \<inter> set (takeCString ys) \<noteq> {}\<rbrakk> \<Longrightarrow> \<exists>ws::string. strpbrk xs ys = Some ws"
+by simp
+
+lemma strpbrk_list_pbrk[simp]:
+"\<lbrakk>isCString xs; isCString ys\<rbrakk> \<Longrightarrow> the (strpbrk xs ys) = list_pbrk (takeCString xs) (takeCString ys)"
+ by simp
+ 
+export_code strpbrk in "Haskell"
+
 end
